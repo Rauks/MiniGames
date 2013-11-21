63efd1811752c6fca15900330b14af31e15882e4
Karl Woditsch <k.woditsch@gmail.com>
2013-11-21 14:59:16 +0100

    Implement game pause toggle

--- Games-Engine/src/net/kirauks/minigames/engine/GameApplication.java
+++ Games-Engine/src/net/kirauks/minigames/engine/GameApplication.java
@@ -6,25 +6,51 @@
 
 package net.kirauks.minigames.engine;
 
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import javafx.util.Duration;
 import net.kirauks.minigames.engine.utils.Options;
 
 /**
  *
  * @author Karl
  */
-abstract class GameApplication extends Application{
+public abstract class GameApplication extends Application{
+    private BorderPane pausePane = new BorderPane();
+    private final static double PAUSE_OPACITY = 0.8d;
+    
     @Override
     final public void start(Stage stage) throws Exception {
         stage.setTitle(this.createStageTitle());
         
-        Scene scene = this.createScene();    
+        StackPane rootPane = new StackPane();
+        this.pausePane.setStyle("-fx-background-color: #000000;");
+        this.pausePane.setFocusTraversable(true);
+        this.pausePane.setOpacity(0.0);
+        this.pausePane.setMouseTransparent(true);
+        Text pauseText = new Text("PAUSE");
+        pauseText.opacityProperty().bind(this.pausePane.opacityProperty().multiply(1.0d / PAUSE_OPACITY));
+        pauseText.setFill(Color.WHITE);
+        this.pausePane.setCenter(pauseText);
+        
+        Scene scene = this.createScene();
+        Parent sceneRoot = scene.getRoot();
+        if(sceneRoot != null){
+            rootPane.getChildren().add(sceneRoot);
+        }
+        rootPane.getChildren().add(pausePane);
+        scene.setRoot(rootPane);
         
         scene.setOnKeyPressed(new EventHandler<KeyEvent>(){
             @Override
@@ -44,6 +70,9 @@ abstract class GameApplication extends Application{
                 }
                 else if(key.equals(Options.getRightKey())){
                     GameApplication.this.onRightKeyPressed();
+                }
+                else if(key.equals(Options.getPauseKey())){
+                    GameApplication.this.togglePause();
                 }
             }
         });
@@ -67,6 +96,26 @@ abstract class GameApplication extends Application{
         });
         
         stage.show();
+    }
+    
+    private boolean paused = false;
+    FadeTransition pauseFade = new FadeTransition(Duration.seconds(0.2d), this.pausePane);
+    private void togglePause() {
+        if(this.paused){
+            this.pauseFade.setFromValue(PAUSE_OPACITY);
+            this.pauseFade.setToValue(0.0d);
+            this.pauseFade.play();
+            this.pausePane.setMouseTransparent(true);
+            this.onResumeGame();
+        }
+        else{
+            this.onPauseGame();
+            this.pausePane.setMouseTransparent(false);
+            this.pauseFade.setFromValue(0.0d);
+            this.pauseFade.setToValue(PAUSE_OPACITY);
+            this.pauseFade.play();
+        }
+        this.paused = !this.paused;
     }
     
     /**
