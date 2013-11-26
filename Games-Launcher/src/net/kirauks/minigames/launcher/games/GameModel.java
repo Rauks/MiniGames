898963bcae93d6f6c66c4ccdfbd12100f3c9799e
Karl Woditsch <k.woditsch@gmail.com>
2013-11-26 17:17:49 +0100

    Fix GameModel serialization

--- Games-Launcher/src/net/kirauks/minigames/launcher/games/GameModel.java
+++ Games-Launcher/src/net/kirauks/minigames/launcher/games/GameModel.java
@@ -6,7 +6,10 @@
 
 package net.kirauks.minigames.launcher.games;
 
-import java.io.Serializable;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 import javafx.beans.property.ReadOnlyStringProperty;
 import javafx.beans.property.SimpleStringProperty;
 
@@ -14,17 +17,23 @@ import javafx.beans.property.SimpleStringProperty;
  *
  * @author Karl
  */
-public class GameModel implements Serializable{
+public class GameModel implements Externalizable{
     private static final long serialVersionUID = 1L;
     
     private SimpleStringProperty name;
     private SimpleStringProperty description;
     private SimpleStringProperty path;
     
+    public GameModel(){
+        this.name = new SimpleStringProperty();
+        this.description = new SimpleStringProperty();
+        this.path = new SimpleStringProperty();
+    }
     public GameModel(String name, String description, String url){
-        this.name = new SimpleStringProperty(name);
-        this.name = new SimpleStringProperty(description);
-        this.name = new SimpleStringProperty(url);
+        this();
+        this.name.setValue(name);
+        this.description.setValue(description);
+        this.path.setValue(url);
     }
     
     public ReadOnlyStringProperty nameProperty(){
@@ -45,5 +54,24 @@ public class GameModel implements Serializable{
     }
     public String getPath(){
         return this.path.getValue();
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput oo) throws IOException {
+        oo.writeObject(this.name.getValue());
+        oo.writeObject(this.description.getValue());
+        oo.writeObject(this.path.getValue());
+    }
+
+    @Override
+    public void readExternal(ObjectInput oi) throws IOException, ClassNotFoundException {
+        this.name.setValue((String) oi.readObject());
+        this.description.setValue((String) oi.readObject());
+        this.path.setValue((String) oi.readObject());
+    }
+    
+    @Override
+    public String toString(){
+        return this.name.getValue();
     }
 }
