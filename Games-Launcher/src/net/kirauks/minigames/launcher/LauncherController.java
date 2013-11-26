e8cd76820e97f169f7d449654ed70de969f6f9db
Karl Woditsch <k.woditsch@gmail.com>
2013-11-26 14:19:15 +0100

    Base of controller

--- Games-Launcher/src/net/kirauks/minigames/launcher/LauncherController.java
+++ Games-Launcher/src/net/kirauks/minigames/launcher/LauncherController.java
@@ -11,26 +11,43 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
 
 /**
  *
  * @author Karl
  */
 public class LauncherController implements Initializable {
+    @FXML
+    private TextArea gameDescription;
+    @FXML
+    private Label gameTitle;
+    @FXML
+    private Button gameStart;
     
     @FXML
-    private Label label;
+    private ListView listGames;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void handleMenuInstall(ActionEvent event) {
+        
+    }
+    @FXML
+    private void handleMenuUninstall(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    private void handleMenuGameStart(ActionEvent event) {
+        
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
 }
