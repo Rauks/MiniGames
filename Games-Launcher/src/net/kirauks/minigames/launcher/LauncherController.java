ec70cd3f872b0d1d5b71d3b0dc2dc0ade7bd24c4
Karl Woditsch <k.woditsch@gmail.com>
2013-11-26 17:02:10 +0100

    Implement test controller

--- Games-Launcher/src/net/kirauks/minigames/launcher/LauncherController.java
+++ Games-Launcher/src/net/kirauks/minigames/launcher/LauncherController.java
@@ -6,8 +6,15 @@
 
 package net.kirauks.minigames.launcher;
 
+import java.io.IOException;
+import net.kirauks.minigames.launcher.games.GameManager;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -16,6 +23,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
+import net.kirauks.minigames.launcher.games.GameModel;
 
 /**
  *
@@ -32,9 +40,15 @@ public class LauncherController implements Initializable {
     @FXML
     private ListView listGames;
     
+    private GameManager manager;
+    
     @FXML
     private void handleMenuInstall(ActionEvent event) {
-        
+        try {
+            this.manager.installGame(new GameModel("Test", "Test description", "test.txt"));
+        } catch (IOException ex) {
+            Logger.getLogger(LauncherController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     @FXML
     private void handleMenuUninstall(ActionEvent event) {
@@ -48,6 +62,16 @@ public class LauncherController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        try {
+            this.manager = GameManager.getInstance();
+        } catch (IOException ex) {
+            Logger.getLogger(LauncherController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        this.listGames.setItems(this.manager.gamesListProperty());
+        
+        this.gameDescription.disableProperty().bind(this.listGames.getSelectionModel().selectedItemProperty().isNull());
+        this.gameTitle.disableProperty().bind(this.listGames.getSelectionModel().selectedItemProperty().isNull());
+        this.gameStart.disableProperty().bind(this.listGames.getSelectionModel().selectedItemProperty().isNull());
     }
 }
