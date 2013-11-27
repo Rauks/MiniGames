6440266924ae78ed7210e418036a1ddf0279a0ac
Karl Woditsch <k.woditsch@gmail.com>
2013-11-27 08:41:53 +0100

    Base gui

--- Games-GsetupWizard/src/games/gsetupwizard/GuiController.java
+++ Games-GsetupWizard/src/games/gsetupwizard/GuiController.java
@@ -11,7 +11,10 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -20,12 +23,19 @@ import javafx.scene.control.Label;
 public class GuiController implements Initializable {
     
     @FXML
-    private Label label;
+    private TextField fileField;
+    @FXML
+    private TextField titleField;
+    @FXML
+    private TextArea descriptionArea;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void handleButtonFile(ActionEvent event) {
+        
+    }
+    @FXML
+    private void handleButtonBuild(ActionEvent event) {
+        
     }
     
     @Override
