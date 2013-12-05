5287e91011fb4e614b1365e74f01c03cbe84d447
Karl Woditsch <k.woditsch@gmail.com>
2013-12-05 17:23:14 +0100

    Launcher launch game

--- Games/Games-BrickBreaker/src/net/kirauks/minigames/brickbreaker/Launcher.java
+++ Games/Games-BrickBreaker/src/net/kirauks/minigames/brickbreaker/Launcher.java
@@ -7,11 +7,6 @@
 package net.kirauks.minigames.brickbreaker;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -21,25 +16,8 @@ import javafx.stage.Stage;
 public class Launcher extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage primaryStage) throws Exception {
+        new Game().start(primaryStage);
     }
 
     /**
