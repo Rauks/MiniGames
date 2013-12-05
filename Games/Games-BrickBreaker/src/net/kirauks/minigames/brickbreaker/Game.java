c3fcaa71029ee5ae5f37b3432372d5cd2fc771c2
Karl Woditsch <k.woditsch@gmail.com>
2013-12-05 17:31:27 +0100

    Set games bases

--- Games/Games-BrickBreaker/src/net/kirauks/minigames/brickbreaker/Game.java
+++ Games/Games-BrickBreaker/src/net/kirauks/minigames/brickbreaker/Game.java
@@ -7,6 +7,7 @@
 package net.kirauks.minigames.brickbreaker;
 
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import net.kirauks.minigames.engine.GameApplication;
 
 /**
@@ -17,57 +18,57 @@ public class Game extends GameApplication{
 
     @Override
     public String createStageTitle() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "Casse Briques";
     }
 
     @Override
     public Scene createScene() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new Scene(new StackPane());
     }
 
     @Override
     public void onCloseStage() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
     public void onPauseGame() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
     public void onResumeGame() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
     public void onActionKeyPressed() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
     public void onActionKeyReleased() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
     public void onUpKeyPressed() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
     public void onDownKeyPressed() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
     public void onLeftKeyPressed() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
     public void onRightKeyPressed() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
     
 }
