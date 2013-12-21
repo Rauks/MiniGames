01645e65856b0aae9bcb25d7c2dc4cc8d1e9fa22
laetitia28 <laetitia.gaignier@gmail.com>
2013-12-21 19:47:29 +0100

    Try JavaFX FXML

---
+++ Games/JavaFXApplication5/src/javafxapplication5/FXMLDocumentController.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javafxapplication5;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Laetitia
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label textLabelChoose;
+    
+    @FXML
+    private void handleButtonActionLevel1(ActionEvent event) {
+        System.out.println("You clicked level 1!");
+        textLabelChoose.setText("Level 1 is choosen");
+    }
+    
+    @FXML
+    private void handleButtonActionLevel2(ActionEvent event) {
+        System.out.println("You clicked me 2 !");
+        textLabelChoose.setText("Level 2 is choosen");
+    }
+        @FXML
+    private void handleButtonActionLevel3(ActionEvent event) {
+        System.out.println("You clicked me 3 !");
+        textLabelChoose.setText("Level 3 is choosen");
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ Games/JavaFXApplication5/src/javafxapplication5/JavaFXApplication5.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapplication5;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author Laetitia
+ */
+public class JavaFXApplication5 extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+
+       
+         Scene scene = new Scene(root, 800, 600, Color.WHITE);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        primaryStage.setTitle("Hangman Start FXML !!! ");
+
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
